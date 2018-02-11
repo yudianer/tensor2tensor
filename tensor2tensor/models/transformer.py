669e7236390d511c20a625f51f175d2b979453c5
jack <yudianer1991@hotmail.com>
2018-02-11 12:30:35 +0800

    ^^

--- tensor2tensor/models/transformer.py
+++ tensor2tensor/models/transformer.py
@@ -1389,10 +1389,12 @@ def transformer_jack():
     hparams = transformer_base_single_gpu()
     hparams.learning_rate_warmup_steps=4000
     hparams.learning_rate=0.01
+    hparams.batch_size=6250
+    hparams.learning_rate_decay_scheme="exp"
     # hparams.num_encoder_layers=6
     # hparams.decode_alpha=0.6
     # hparams.label_smoothing=0.1
-    hparams.optimizer_adam_beta2=0.98
+    # hparams.optimizer_adam_beta2=0.98
     hparams.initializer="normal_unit_scaling"
     # hparams.num_decoder_layers=6
     # hparams.shared_source_target_embedding=False
